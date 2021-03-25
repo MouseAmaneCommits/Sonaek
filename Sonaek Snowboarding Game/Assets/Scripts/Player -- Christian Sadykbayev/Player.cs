ca2dc61872e04cbd7c88b649c3430d29cddfaecb
chris-chan-commits <48699812+chris-chan-commits@users.noreply.github.com>
2021-03-25 11:15:55 -0700

    A little bit of work on the player script
    I worked on the player script a little

---
+++ Sonaek Snowboarding Game/Assets/Scripts/Player -- Christian Sadykbayev/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// LAST EDITED ---- CHRISTIAN SADYKBAYEV
+public class Player : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Sonaek Snowboarding Game/Assets/Scripts/Player -- Christian Sadykbayev/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// LAST EDITED ---- CHRISTIAN SADYKBAYEV
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody player;
+    public float speed;
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            player.AddForce(player.transform.forward * speed * Time.deltaTime);
+        }
+    }
+}
