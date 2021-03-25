49639474281cd8c091193adbe4e5b00162004651
chris-chan-commits <48699812+chris-chan-commits@users.noreply.github.com>
2021-03-25 13:37:50 -0700

    Fixed the point system
    Fixed the point system

---
+++ Sonaek Snowboarding Game/Assets/Scripts/PointSys -- Ben-Christian/PointSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class PointSystem : MonoBehaviour
+{
+    public Text CounterRef;
+    public GameObject PlayerRef;
+    public float Points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CounterRef.text = "Points: " + Points;
+    }
+}
