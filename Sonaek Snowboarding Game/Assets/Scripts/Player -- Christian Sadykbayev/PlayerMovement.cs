ff56f9caeb08e1a92f0c1257b88212c39b7cf20c
chris-chan-commits <48699812+chris-chan-commits@users.noreply.github.com>
2021-03-25 12:34:30 -0700

    Added more player code
    However, it would be helpful if anybody will fix the slowing down mechanic(i removed it) because when you slow down, and you go to a stop, it will go backwards.

--- Sonaek Snowboarding Game/Assets/Scripts/Player -- Christian Sadykbayev/PlayerMovement.cs
+++ Sonaek Snowboarding Game/Assets/Scripts/Player -- Christian Sadykbayev/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody player;
     public float speed;
+    public float turnSpeed;
+    private float xRotation;
 
     
     void Start()
@@ -14,11 +16,28 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
+    public void resetPool(Rigidbody rg)
+    {
+        rg.velocity = Vector3.zero;
+        rg.angularVelocity = Vector3.zero;
+    }
+
     void Update()
     {
+        // Move Forward
         if(Input.GetKey(KeyCode.W))
         {
             player.AddForce(player.transform.forward * speed * Time.deltaTime);
+        }
+
+        // STOP
+        if(Input.GetKey(KeyCode.S))
+        {
+            // INSERT ANIMATION HERE
+
+            // !INSERT ANIMATION HERE
+
+            
         }
     }
 }
