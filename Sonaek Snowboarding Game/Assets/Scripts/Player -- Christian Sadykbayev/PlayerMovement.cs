412f19d9b07b250daff340e857b154bb8ead3fdc
Ben <Theultramadman@gmail.com>
2021-03-25 20:08:23 -0700

    Made board be more like board

--- Sonaek Snowboarding Game/Assets/Scripts/Player -- Christian Sadykbayev/PlayerMovement.cs
+++ Sonaek Snowboarding Game/Assets/Scripts/Player -- Christian Sadykbayev/PlayerMovement.cs
@@ -7,9 +7,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody player;
+    public GameObject SnowboardRef;
     public float speed;
     public float turnspeed; // Don't change this to a high amount or else its spinny boi
     private float xRotation;
+    private float MaxVelocity = 65;
 
 
 
@@ -19,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
         // DON'T TOUCH THIS, this is for making the board not flip when moving
         var SetSnowBoardCentreGravity = new Vector3(0, -2, 0);
         player.centerOfMass = SetSnowBoardCentreGravity;
-
+        
     }
 
     public void resetPool(Rigidbody rg)
@@ -30,6 +32,24 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+
+        bool hit = Physics.Raycast(SnowboardRef.transform.position, Vector3.down, 1);
+        Debug.DrawRay(SnowboardRef.transform.position, Vector3.down, Color.red, 1);
+        player.velocity = Vector3.ClampMagnitude(player.velocity, MaxVelocity);
+
+        if (hit == true)
+        {
+            print("YOUR HITTING THE GROUND");
+            player.constraints = RigidbodyConstraints.None;
+        }
+        else
+        {
+            print("You're in the air");
+            transform.position = transform.position -= new Vector3(0, 0.2f);
+            player.constraints = RigidbodyConstraints.FreezeRotation;
+            player.rotation.SetEulerAngles(0, 0, 0);
+        }
+
         // Move Forward
         if (Input.GetKey(KeyCode.W))
         {
