5618e5145c9a763c694460c670d93bde4c53576e
chris-chan-commits <48699812+chris-chan-commits@users.noreply.github.com>
2021-03-25 21:56:49 -0700

    A lot of crap
    Added drag in the air, added drag when   your on the ground, added drag when you powerslide, and added powersliding (duh).

---
+++ Sonaek Snowboarding Game/Assets/Scripts/PointSys/PointSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+// LAST EDITED : Christian Sadykbayev
+public class PointSystem : MonoBehaviour
+{
+    public Text CounterRef;
+    public GameObject PlayerRef;
+    public float Points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the points to the current points of the player
+        CounterRef.text = "Points: " + Points;
+    }
+}
