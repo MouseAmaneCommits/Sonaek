220851208cf86e2e99a3f42656f5e68d2459f48f
Ian Botashev <33396704+IanBotashev@users.noreply.github.com>
2021-03-27 16:59:25 -0700

    Oh god, oh lord, the thing is done.
    tricks added, and i suffered a lot. thank you for coming to my ted talk.

--- Sonaek Snowboarding Game/Assets/Scripts/Tricks/Trick.cs
+++ Sonaek Snowboarding Game/Assets/Scripts/Tricks/Trick.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SampleTrick", menuName = "Tricks/Sample Trick")]
 public class Trick : ScriptableObject
 {
+    // General Settings about Trick
+    public string name = "Sample Trick";
+
     // Requirements for trick to be completed
     public float requiredYSpin = 0;
-    public bool whileGrounded = false;
+    public float requiredXSpin = 0;
+
+    public bool done;
 }
--- Sonaek Snowboarding Game/Assets/Scripts/Tricks/TrickManager.cs
+++ Sonaek Snowboarding Game/Assets/Scripts/Tricks/TrickManager.cs
@@ -7,28 +7,74 @@ public class TrickManager : MonoBehaviour
     public List<Trick> tricks;
     public PlayerMovement playerMovement;
     public MouseLook mouseLook;
+
     private float totalYRotation = 0;
+    private float totalXRotation = 0;
+
+    private bool inAir = false;
 
     // Update is called once per frame
     void Update()
     {
+
+
         // If we are in the air.
         if (!playerMovement.isGrounded())
         {
+            inAir = true;
 
             totalYRotation += Input.GetAxis("Mouse X") * mouseLook.mouseSensitivity * Time.deltaTime;
+            totalXRotation += Input.GetAxis("Mouse Y") * mouseLook.mouseSensitivity * Time.deltaTime;
+
             foreach (Trick trick in tricks)
             {
-                if (totalYRotation >= trick.requiredYSpin && trick.requiredYSpin != 0)
+                if ( checkDidTrick(trick) && !trick.done)
                 {
-                    Debug.Log("Trick done.");
-                    tricks.Remove(trick);
+                    Debug.Log("Trick done: " + trick.name);
+                    trick.done = true;
                 }
             }
         }
         else
         {
+            // If this is the first frame we are on the ground
+            if (inAir)
+            {
+                setTricksDoneStatus(false);
+            }
+            inAir = false;
             totalYRotation = 0;
+            totalXRotation = 0;
+        }
+    }
+
+    bool checkDidTrick(Trick trick)
+    {
+        bool didYSpin = false;
+        bool didXSpin = false;
+
+        // Forgive me father, for i have sinned, i made extremely long if statements.
+
+        float numSign = Mathf.Sign(trick.requiredYSpin);
+        if ((totalYRotation >= trick.requiredYSpin && numSign == 1) || (totalYRotation <= trick.requiredYSpin && numSign == -1) || trick.requiredYSpin == 0)
+        {
+            didYSpin = true;
+        }
+
+        numSign = Mathf.Sign(trick.requiredXSpin);
+        if ((totalXRotation >= trick.requiredXSpin && numSign == 1) || (totalXRotation <= trick.requiredXSpin && numSign == -1) || trick.requiredXSpin == 0)
+        {
+            didXSpin = true;
+        }
+
+        return (didXSpin && didYSpin);
+    }
+
+    void setTricksDoneStatus(bool setValue)
+    {
+        foreach (Trick trick in tricks)
+        {
+            trick.done = setValue;
         }
     }
 }
