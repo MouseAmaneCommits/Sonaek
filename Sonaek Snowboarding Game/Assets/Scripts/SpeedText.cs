2f36c4940a2cd60b58abbb272e21cb70183b55e3
Ian Botashev <33396704+IanBotashev@users.noreply.github.com>
2021-03-28 12:33:28 -0700

    Added a speed meter

---
+++ Sonaek Snowboarding Game/Assets/Scripts/SpeedText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedText : MonoBehaviour
+{
+    public Rigidbody player;
+    public Text text;
+
+    void Update()
+    {
+        text.text = "Speed: " + Mathf.Round(player.velocity.magnitude);    
+    }
+}
